4e008f151972d4568abd44ef00d5290ef3e83d54
sdelgado0125 <sdelgado0125@gmail.com>
2023-10-24 12:52:40 -0400

    updated test

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,16 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  const values = { amount: 10000, years: 10, rate: 4.5 };
+    const result = calculateMonthlyPayment(values);
+    expect(result).toBeCloseTo(93.22, 2); 
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  const values = { amount: 10000, years: 10, rate: 4.5 };
+  const result = calculateMonthlyPayment(values);
+  const decimalPlaces = (result.split('.')[1] || '').length;
+  expect(decimalPlaces).toBe(2);
 });
 
-/// etc
+
